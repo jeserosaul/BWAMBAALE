584d8ce0df1d1178a0bd6bea0d230a11c3261896
jeserosaul <vjsoul97@gmail.com>
2024-11-27 14:25:12 +0300

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,4 @@
+function Hello(){
+    alert("please be with a faithful heat of being helpful,and lets support the young generation especially the ones who can access their needs of life!!!(thank you)")
+    
+}
